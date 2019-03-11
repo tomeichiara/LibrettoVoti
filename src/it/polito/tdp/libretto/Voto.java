6d537401facdcb9b3e7d6885330b8e856588a5b1
chiar <chiar@ComputerChiaraTorino>
2019-03-11 13:53:10 +0100

    aggiunta metodi standard

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -7,5 +7,31 @@ public class Voto {
 	private int voto;
 	private String corso;
 	private LocalDate data;		// per le date!!
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
 	
 }
